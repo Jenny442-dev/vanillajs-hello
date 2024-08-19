61f00d1aa44777e4bd3a6749bd4b361073d6ba2e
Jenny442-dev <Jennywagner442@gmail.com>
2024-08-19 14:23:07 +0000

    Initial commit

--- src/jenna.js
+++ src/jenna.js
@@ -7,9 +7,10 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  import "./jenna.js";
+  import "./app.js";
 
   import "./assets/img/OIP";
   import "./assets/img/R";
+  import "./assets/img/111509-landscape-nature";
   console.log("Hello Rigo from the console!");
 };
