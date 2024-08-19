671e8cd8e0081123dcdd5aa747419f03ca7f695c
Jenny442-dev <Jennywagner442@gmail.com>
2024-08-19 14:23:07 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { ESLint } from "eslint";
 
 window.onload = function() {
   //write your code here
@@ -11,5 +12,6 @@ window.onload = function() {
 
   import "./assets/img/OIP";
   import "./assets/img/R";
+  ESLint;
   console.log("Hello Rigo from the console!");
 };
